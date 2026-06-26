1d5c2904dad4dfcf3e5da097e8e9d7ac4f85e549
shaoyuca <59860059+shaoyuca@users.noreply.github.com>
2026-06-26 23:14:14 +0800

    Deploy root about page

--- _next/static/chunks/app/courses/page-7248e0daa4b10500.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[65,301,444,626],{4667:function(e,n,s){Promise.resolve().then(s.t.bind(s,5878,23)),Promise.resolve().then(s.t.bind(s,2972,23))}},function(e){e.O(0,[972,878,971,117,744],function(){return e(e.s=4667)}),_N_E=e.O()}]);
